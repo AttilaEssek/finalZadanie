15ef17f4f1c4bba55512cee66abd8af935c60898
AttilaEssek <xessek@stuba.sk>
2023-01-02 18:05:07 +0100

    final mazhaps

--- project/sw.js
+++ project/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", e => {
         return cache.addAll(["./","./scripts/cookies.js","./styles/style.css","./scripts/easy.json","./scripts/elements.js","./scripts/hard.json","./scripts/script1.js","./scripts/script2.js","./images/angle.svg","./images/arrowDown.png","./images/arrowUp.png","./images/cross.svg","./images/from.svg","./images/from2.svg","./images/straigt.svg","./images/tail.svg","./images/to.svg","./images/to2.svg","./images/triangle.svg","./images/wall2.jpg","./lib/bootstrap.bundle.min.js","./lib/bootstrap.bundle.min.js.map","./lib/bootstrap.min.css","./lib/bootstrap.min.css.map","./lib/jquery-3.6.0.min.js"]);
         })
     );
-    console.log("install");
+    console.log("install!!!");
 });
 
 self.addEventListener("fetch", e =>{
